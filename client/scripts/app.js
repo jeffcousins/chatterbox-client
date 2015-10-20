ff59912c4a8d44ac06e08114071731bc63a6487d
Jeff Cousins <jeffcousins@me.com>
2015-10-19 21:47:43 -0500

    started project

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,37 @@
 // YOUR CODE HERE:
 
+
+
+
+
+
+
+var App = Parse.Object.extend('App', {what:'name'});
+
+var app = new App();
+
+var displayData = function(messages) {
+  messages.forEach(function(message) {
+    var messageBox = $('<div>').attr('class', 'chat');
+    var userBox = $('<p>').attr('class', 'username').text(message.username || 'anonymous');
+    var textBox = $('<p>').text(message.text);
+    messageBox.append(userBox).append(textBox);
+    $('#main').append(messageBox);
+  });
+};
+
+var updateData = function() {
+  $.ajax({
+    type: 'GET',
+    url: "https://api.parse.com/1/classes/chatterbox",
+    data: 'order=-createdAt',
+    contentType: "application/json",
+    success: function(response) {
+      getData = response.results;
+      displayData(response.results);
+    }
+  });
+};
+
+updateData();
+setInterval(updateData, 30000);
